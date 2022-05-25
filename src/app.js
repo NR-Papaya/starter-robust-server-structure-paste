1e3b5cbb167f25020c2479d8d57c49b0a136abcf
Nahia Riviera <n.riviera.dev@gmail.com>
2022-05-25 13:46:13 -0500

    restful APIs

--- src/app.js
+++ src/app.js
@@ -2,8 +2,36 @@ const express = require("express");
 const app = express();
 const pastes = require("./data/pastes-data");
 // TODO: Follow instructions in the checkpoint to implement ths API.
+
+//built in middleware that adds a body property to the request req.body
+app.use(express.json());
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
 app.get("/pastes", (req, res, next) => {
 	res.json({ data: pastes });
+});
+
+app.post("/pastes", (req, res, next) => {
+	const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+		req.body;
+	if (text) {
+		const newPaste = {
+			id: ++lastPasteId, // Increment last ID, then assign as the current ID
+			name,
+			syntax,
+			exposure,
+			expiration,
+			text,
+			user_id,
+		};
+		pastes.push(newPaste);
+		res.status(201).json({ data: newPaste });
+	}else{
+    res.sendStatus(400)
+  }
 });
 
 app.get("/pastes/:pasteId", (req, res, next) => {
